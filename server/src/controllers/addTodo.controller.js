2eb0a8e7a2fdda8fd8334e7a2be728485e56da0e
? <=>
2024-04-30 22:09:55 +0500

    FINSISHED TODO IN SERVER PROBABLY ADD USER LOGIN/SIGNUP OTHERWISE MAYBE CREATING CLIENT TOMORROW

---
+++ server/src/controllers/addTodo.controller.js
@@ -0,0 +1,18 @@
+import { Todo } from "../models/todo.model.js"
+const addTodo = async (req,res) => {
+    try{
+        const {todoTitle , todoDescription, todoBody } = req.body;
+        const newTodo = new Todo({
+            todoTitle,
+            todoDescription,
+            todoBody
+        });
+        await newTodo.save(); // Save the newTodo to the database
+
+        console.log("Todo added successfully");
+        res.status(201).json({ msg: "Todo added successfully", newTodo });
+    }catch (e){
+        res.json({msg : "err occured while adding Todo to the database",error : {isError : true , error : e}});
+    }
+};
+export default addTodo;
---
+++ server/src/routes/todo.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import multer from "multer";
+import addTodo from "../controllers/addTodo.controller.js";
+import getTodo from "../controllers/getTodo.controller.js";
+import clearDatabase from "../controllers/clearDatabase.controller.js";
+import updateTodo from "../controllers/updateTodo.controller.js";
+const todoRouter = Router();
+const upload = multer({dest : "./server/public"})
+todoRouter.route("/addTodo").post(upload.none(),addTodo);
+todoRouter.route("/getTodo").get(upload.none(),getTodo);
+todoRouter.route("/clearDb").delete(upload.none(),clearDatabase);
+todoRouter.route("/updateTodo").put(upload.none(),updateTodo);
+todoRouter.route("/deleteTodo").delete(upload.none(),updateTodo);
+export default todoRouter;
